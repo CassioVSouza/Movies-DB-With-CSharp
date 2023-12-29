682a03448f3f7f659446db0070683ace3bcfa573
Cássio Souza <cassiosouza248@gmail.com>
2023-12-29 12:23:18 -0300

    Account Creation, Login and Cryptography Password

---
+++ AccountAPI/AccountManagement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountAPI
+{
+    public class AccountManagement
+    {
+        public AccountModel RegisterAccount()
+        {
+            AccountModel Account = new AccountModel();
+            Console.WriteLine("Tell us your name: ");
+            Account.Name = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Tell us your email: ");
+            Account.Email = Console.ReadLine() ?? "";
+
+            Console.WriteLine("Tell us your password: ");
+            Account.Password = (Console.ReadLine()) ?? "";
+
+            Console.WriteLine("Tell us your Phone Number: ");
+            Account.PhoneNumber = Console.ReadLine() ?? "";
+
+            if (CheckNull(Account))
+            {
+                Console.WriteLine("Registering Account...");
+                Account.SucessfullyCreated = true;
+                return Account;
+            }
+            else
+            {
+                Console.WriteLine("You can't let fields empty!");
+            }
+            return Account;
+ 
+        }
+
+        public string LoginAccountEmail()
+        {
+            Console.Write("Insert your email: ");
+            string Email = Console.ReadLine() ?? string.Empty;
+            return Email;
+        }
+
+        public string LoginAccountPassword()
+        {
+            Console.Write("Insert your password: ");
+            string Password = Console.ReadLine() ?? string.Empty;
+            return Password;
+        }
+
+        private bool CheckNull(AccountModel Account)
+        {
+            return !string.IsNullOrWhiteSpace(Account.Name) && !string.IsNullOrWhiteSpace(Account.Email) &&
+                   !string.IsNullOrWhiteSpace(Account.PhoneNumber) && !string.IsNullOrWhiteSpace(Account.Password) ? true : false; ;
+        }
+    }
+}
--- Movies/Program.cs
+++ Movies/Program.cs
@@ -2,6 +2,10 @@
 using MoviesLibrary;
 using System.Data.Entity.Migrations.Model;
 using System.Data.SQLite;
+using BCrypt.Net;
+using AccountAPI;
+using System.Security.Cryptography.X509Certificates;
+using System.Data.SqlClient;
 
 namespace Movies
 {
@@ -12,15 +16,61 @@ namespace Movies
             string ConnectionString = "Data Source=D:/EstudandosBancos/EstudandoBancos.db;Version=3;";
 
             DataBaseManager dataBaseManager = new DataBaseManager(ConnectionString);
-            MoviesManager moviesDB = new MoviesManager();
-            int Choice;
+            AccountManagement accountManagement = new AccountManagement();
 
             dataBaseManager.InitializeDataBase();
+            Console.Write("Welcome to the system, do you want to:\n1 - Register\n2 - Login\n3 - Exit");
+            int AccountChoice = Convert.ToInt32(Console.ReadLine());
+
             try
             {
+                switch (AccountChoice)
+                {
+                    case 1:
+                        dataBaseManager.CreateAccount(accountManagement.RegisterAccount());
+                        break;
+
+                    case 2:
+                        if (dataBaseManager.CheckPassword(dataBaseManager.CheckEmailAccount()))
+                        {
+                            Console.WriteLine("Welcome!");
+                            MoviesOptions(ConnectionString);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Attempt to login failed!");
+                        }
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Exited!");
+                        break;
+                    default:
+                        Console.WriteLine("Insert a valid number!");
+                        break;
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                dataBaseManager.CloseDatabase();
+            }
+        }
+
+        private static void MoviesOptions(string ConnectionString)
+        {
+            DataBaseManager dataBaseManager = new DataBaseManager(ConnectionString);
+            MoviesManager moviesDB = new MoviesManager();
+
+            try
+            {
+                dataBaseManager.InitializeDataBase();
+                int Choice;
                 do
                 {
-                    Console.WriteLine("Welcome, What do you want to do?\n1 - Add Movie\n2 - Show Movies\n3 - Edit Movie\n4 - Delete Movie");
+                    Console.WriteLine("Welcome, What do you want to do?\n1 - Add Movie\n2 - Show Movies\n3 - Edit Movie\n4 - Delete Movie\n5- Exit");
                     Choice = Convert.ToInt32(Console.ReadLine());
 
                     switch (Choice)
@@ -39,13 +89,16 @@ namespace Movies
                             dataBaseManager.ShowMoviesDB();
                             dataBaseManager.DeleteMovie();
                             break;
+                        case 5:
+                            Console.WriteLine("Exited!");
+                            break;
                         default: Console.WriteLine("Insert a valid number!");
                             break;
                     }
 
-                }while(Choice != 5);
+                } while (Choice != 5);
             }
-            catch (FormatException ex) 
+            catch (FormatException ex)
             {
                 Console.WriteLine(ex.Message);
             }
@@ -160,10 +213,105 @@ namespace Movies
             }
         }
 
+        public void CreateAccount(AccountModel Account)
+        {
+            PasswordManager passwordManager = new PasswordManager();
+
+            string SQLiteCreateCommand = "INSERT INTO Accounts (Name, Email, Password, PhoneNumber) VALUES (@Name, @Email, @Password, @PhoneNumber)";
+            using (SQLiteCommand command = new SQLiteCommand(SQLiteCreateCommand, _connection)) 
+            {
+                  command.Parameters.AddWithValue("@Name", Account.Name);
+                  command.Parameters.AddWithValue("@Email", Account.Email);
+                  command.Parameters.AddWithValue("@Password", passwordManager.HashPassword(Account));
+                  command.Parameters.AddWithValue("@PhoneNumber", Account.PhoneNumber);
+                  int RowsAffected = command.ExecuteNonQuery();
+
+                  if (RowsAffected > 0)
+                  {
+                      Console.WriteLine("Account created sucessfully");
+                  }
+                  else {
+                      Console.WriteLine("An error happened!");
+                  }
+            }
+        }
+        public string CheckEmailAccount()
+        {
+            AccountManagement accountManagement = new AccountManagement();
+            try
+            {
+                string SQLiteSelectCommand = "SELECT * FROM Accounts WHERE Email = @Email";
+                bool AccountFounded = false;
+                do
+                {
+                    using (SQLiteCommand command = new SQLiteCommand(SQLiteSelectCommand, _connection))
+                    {
+                        string Email = accountManagement.LoginAccountEmail();
+
+                        command.Parameters.AddWithValue("@Email", Email);
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine($"Account finded!");
+                                AccountFounded = true;
+                                return Email;
+                            }
+                        }
+                    }
+                } while (AccountFounded == false);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return "This email is not registered!";
+        }
+
+        public bool CheckPassword(string Email)
+        {
+            AccountManagement accountManagement = new AccountManagement();
+            PasswordManager passwordManager = new PasswordManager();
+
+            string SQLiteSelectCommand = "SELECT * FROM Accounts WHERE Email = @Email";
+            using(SQLiteCommand command = new SQLiteCommand(SQLiteSelectCommand, _connection))
+            {
+                command.Parameters.AddWithValue("@Email", Email);
+                using(SQLiteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string DBPassword = reader["Password"].ToString() ?? string.Empty;
+                        string UserPassword = accountManagement.LoginAccountPassword();
+
+                        return passwordManager.CheckPassword(DBPassword, UserPassword) ? true : false;
+                    }
+                }
+            }
+            return false;
+        }
+
         internal void CloseDatabase()
         {
             _connection.Close();
         }
 
+    }
+
+    class PasswordManager
+    {
+        internal string HashPassword(AccountModel Account)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+
+            string HashPassword = BCrypt.Net.BCrypt.HashPassword(Account.Password, salt);
+
+            return HashPassword;
+        }
+
+        internal bool CheckPassword(string DBPassword, string UserPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(UserPassword, DBPassword);
+        }
     }
 }
